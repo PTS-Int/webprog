f1923eaadbc4dda0071ee5ba0732e15031605b84
PTS-Int <ek_kamol@hotmail.com>
2016-08-28 01:34:46 +0700

    ready to use

--- newroute/script.js
+++ newroute/script.js
@@ -1,5 +1,7 @@
 var glomkarnURL = 'https://raw.githubusercontent.com/PTS-Int/webprog/master/newroute/geojson/';
 var routeList = [];
+var kml = [];
+var map;
 routeList[0] = [29, 34, '34E', 39, '39A', 52, 59, '59A', '59E', 95, '95A', 
                 '106E', '107E', 126, '129E', 150, 154, 185, 187, 
                 208, 209, 210, 227, 228, 229, 230, 231, 256,
@@ -7,11 +9,11 @@ routeList[0] = [29, 34, '34E', 39, '39A', 52, 59, '59A', '59E', 95, '95A',
                 'A1', 'A2'];
 routeList[1] = [26, '26E', 27, 60, 71, 73, 92, 93, 94, 96, '96A',
                 113, 115, 130, 131, 143, '143A', 144, 152, 168, 182, 190, 197, 198, 199,
-                200, 212, 213, '213A', 214, 215, 216, 217, 218, 219, 220, 224, 225, 226,
+                200, 212, 213, '213A', 215, 216, 217, 218, 219, 220, 224, 225, 226,
                 501, '512E', 514, '514E', 517, 519, 525, 526, 527, 549 ,550];
 routeList[2] = [2, '2E', '11A', 23, '23E', 25, '25E', 38, '45E', 46, 48,
                 '102E', 116, 132, 133, '139E', '142E', 145, '145A', '145E', 162, '180E', '181E',
-                207, 214, 222, 223, 232, 244, 234, 235,
+                207, 214, 222, 223, 232, 234, 235,
                 '511E', '513E', '536E', '537E', 548, 551, 552, 553];
 routeList[3] = [1, 4, 12, 13, 14, 22, 36, 47, 62, 67, 72, 77, 78, 99,
                 100, 136, 137, 176, 179, 195,
@@ -37,12 +39,11 @@ routeList[7] = [3, 8, 24, '36A', 44, 49, 54, 55, '73A',
                 'B', 'B1', 'B2', 'B3'];
 
 function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
+        map = new google.maps.Map(document.getElementById('map'), {
           zoom: 10,
           center: {lat: 13.8478485, lng: 100.5558137}
         });
 
-        var kml = [];
         for (var i = 0; i < routeList.length; i++) {
             kml[i] = [];
             for (var j = 0; j < routeList[i].length; j++) {
@@ -52,3 +53,81 @@ function initMap() {
             }
         }
       }
+
+$(document).ready(function() {
+    var html = '<table col="5">';
+    var counter = 0;
+    for (var i = 0; i < routeList.length; i++) {
+        html += '<tr id="' + (i + 1) + '"><td colspan="5"><label><input onclick="event.stopPropagation();" class="khet allKhet' + (i + 1) + '" type="checkbox" value="' + i + '" checked>เขต' + (i + 1) + '</label><button type="button" class="rowtoggle">Hide</button></td></tr>';
+        for (var j = 0; j < routeList[i].length; j++) {
+            if(j % 5 == 0) {
+                html += '<tr class="rowChild' + (i + 1) + '">';
+            }
+            html += '<td><input type="checkbox" class="route khet' + (i + 1) + '" value="' + routeList[i][j] + '" checked>' + routeList[i][j] + '</td>';
+            if(j % 5 == 4) {
+                html += '</tr>';
+            }
+            counter++;
+        }
+        if(routeList[i].length % 5 != 0) {
+            html += '</tr>';
+        }
+        html += '<tr></tr>';
+    }
+    html += '</table>';
+    $('#sideBar').html(html);
+
+    $('.rowtoggle').on('click', function() {
+        $('.rowChild'+$(this).parent().parent().attr('id')).toggle();
+        if($(this).text() == 'Hide') {
+            $(this).text('Show');
+        }
+        else {
+            $(this).text('Hide');
+        }
+    });
+
+    $('input[type=checkbox].khet').on('change', function() {
+        if($(this).prop("checked")) {
+            var khet = $(this).parent().parent().parent().attr('id');
+            $(this).parent().parent().parent().parent().find('.khet' + khet).prop('checked', true);
+            for (var i = 0; i < kml[khet - 1].length; i++) {
+                kml[khet - 1][i].setMap(map);
+            }
+        } else {
+            var khet = $(this).parent().parent().parent().attr('id');
+            $(this).parent().parent().parent().parent().find('.khet' + khet).prop('checked', false);
+            for (var i = 0; i < kml[khet - 1].length; i++) {
+                kml[khet - 1][i].setMap(null);
+            }
+        }
+    });
+
+    $('input[type=checkbox].route').on('change', function() {
+        var thisRoute = $(this).val();
+        var found = false;
+        for (var i = 0; i < routeList.length && !found; i++) {
+            for (var j = 0; j < routeList[i].length; j++) {
+                if(thisRoute == routeList[i][j]) {
+                    if($(this).prop("checked")) {
+                        for (var k = 1; k < 9; k++) {
+                            if ($('.khet' + k + ':checked').length == $('.khet' + k).length) {
+                                $('.allKhet' + k).prop('checked', true);
+                            }
+                        }
+                        kml[i][j].setMap(map);
+                    } else {
+                        for (var k = 1; k < 9; k++) {
+                            if ($('.khet' + k + ':checked').length != $('.khet' + k).length) {
+                                $('.allKhet' + k).prop('checked', false);
+                            }
+                        }
+                        kml[i][j].setMap(null);
+                    }
+                    found = true;
+                    break;
+                }   
+            }
+        }
+    });
+});
